31e9302d18f84fa234b591ea80310413d01cbd1a
Ademir Mazer Junior <mazer@uepg.br>
2015-06-29 09:54:53 -0300

    Nodejs, gulp, bower, npm installs

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# angularagrant
+#
+# author: Ademir Mazer Jr <ademir.mazer.jr@gmail.com
+# url: https://github.com/nunomazer/angularagrant
+
+
+# NODE.JS
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C7917B12
+sudo sh -c 'echo "deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu trusty main" >> /etc/apt/sources.list.d/nodejs.list'
+
+sudo apt-get update
+
+# INSTALL Node.js node - recess - gulp - bower
+sudo apt-get install nodejs
+sudo npm install -g recess
+npm install -g gulp
+npm install -g bower
+ 
+
+
